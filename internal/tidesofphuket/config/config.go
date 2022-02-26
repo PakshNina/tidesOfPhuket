09e1090b7ddcb9c748c6820be1473d1632da58b7
pakshnina <kuz.nina.ko@gmail.com>
2022-02-26 13:36:06 +0300

    Config

--- internal/tidesofphuket/config/config.go
+++ internal/tidesofphuket/config/config.go
@@ -14,9 +14,9 @@ type Config struct {
 func NewTidesOfPhuketConfig() (*Config, error) {
 	cfg := Config{}
 	tools.EnvToString(&cfg.WorldTideApiKey, "WORLD_TIDE_API_KEY", "")
-	tools.EnvToString(&cfg.WorldTideUrl, "WORLD_TIDE_URL", "https://www.worldtides.info/api/v3")
+	tools.EnvToString(&cfg.WorldTideUrl, "WORLD_TIDE_URL", "")
 	tools.EnvToString(&cfg.TelegramToken, "WORLD_TIDE_TELEGRAM_TOKEN", "")
-	tools.EnvToString(&cfg.RedisAddr, "WORLD_TIDE_REDIS_ADDR", "127.0.0.1:6379")
+	tools.EnvToString(&cfg.RedisAddr, "WORLD_TIDE_REDIS_ADDR", "")
 	tools.EnvToString(&cfg.RedisPassword, "WORLD_TIDE_REDIS_PASSWORD", "")
 	return &cfg, nil
 }
